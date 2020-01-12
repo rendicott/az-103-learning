db1579e97b7024365cd204486bb01720760d61c0
Russell Endicott <rendicott@gmail.com>
2020-01-12 11:28:54 -0500

    got some decent createVM functions and added some notes to README

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # az-103 learning
 Scripts and materials used while studying for the AZ-103 Microsoft Azure Administrator 2020 exam. 
+
+
+# Takeaways so far
+* creating a windows VM takes at least 15 minutes
+* updating permissions on a Managed Service Identity (MSI) takes at least 30 minutes
+* Azure Powershell module googling is a confusing wasteland of old references to `AzureRm` modules that don't work anymore.
+* Powershell isn't very helpful when you give a cmdlet the wrong parameters
+* Azure networking is wonky.
+   * I don't know why I can't see subnets, NSG's, route tables, and VNets all in the same console window. I have to have 4 tabs open to get a good picture. 
+   * The effective routes view takes at least 10 minutes to load which is trash. At least if you're not going to show me what my actual route table looks like then make the effective routes view not take 10 minutes to load.
+* It seems to be impossible to load balance an existing set of VMs unless you had enough foresight to put them in an availability set before launching them. This is wacky.
+* If I create an object called "Public IP Address" and I select "Dynamic" you should probably give me a warning that says I won't actually get a public IP until I associate rules to the load balancer. 
+* Why doesn't the console have bulk actions? When I select multiple items from a list of network interfaces I should be able to delete all of them. 
+
+# Handy Powershell Commands
+
+Get Private IP of a VM
+```powershell
+$vm = get-azvm -name web1
+AGet-AzNetworkInterface | where-object {$_.VirtualMachine.Id -eq $vm.Id} | foreach-object {$_.IpConfigurations.PrivateIpAddress }
+```
+
+With Azure CLI
+```
+az vm show -d -n web1 --resource-group webfarm --query privateIps
+```
--- powershell/create-linux.ps1
+++ powershell/create-linux.ps1
@@ -1,36 +1,82 @@
-$cred = Get-Credential
-## Azure Account
-$LocationName = "eastus2"
+
 $ResourceGroupName = "webfarm"
 
-## VM
-$ComputerName = "web1"
-$VMName = "web1"
-## Get-AzVMSize -Location eastus | Sort-Object -Property MemoryInMB | select -First 10
-$VMSize = "Standard_B1s"
+function Add-LinuxVMs {
+    param(
+        [string]$VMUserdata
+    )
+    Add-LinuxVM -VMName "web1" -VMSubnet "web-1" `
+        -VMUserdata $VMUserdata -VMKey "../misc/id_rsa.pub" `
+        -VMAvailabilitySet "webset"
+    Add-LinuxVM -VMName "web2" -VMSubnet "web-2" `
+        -VMUserdata $VMUserdata -VMKey "../misc/id_rsa.pub" `
+        -VMAvailabilitySet "webset"
+}
+function Remove-LinuxVMs {
+    Remove-VM -VMName "web1"
+    Remove-VM -VMName "web2"
+}
 
-## Networking
-$subnet1 = Get-AzVirtualNetwork -name $vnet.name | Get-AzVirtualNetworkSubnetConfig | where-object {$_.name -eq "web-1" }
-$NIC = New-AzNetworkInterface `
-                    -Name "" -ResourceGroupName $ResourceGroupName -Location $LocationName -SubnetId $subnet1.Id
-$VirtualMachine = New-AzVMConfig -VMName $VMName -VMSize $VMSize
-$VirtualMachine = Set-AzVMOperatingSystem `
-                    -VM $VirtualMachine `
-                    -Windows `
-                    -ComputerName $ComputerName `
-                    -Credential $cred `
-                    -ProvisionVMAgent `
-                    -EnableAutoUpdate
-$VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -NetworkInterface $NIC
-#  Get-AzVMImageSku -location eastus -publishername MicrosoftWindowsServer -offer WindowsServer 
-#  Get-AzVMImageSku -location eastus -publishername MicrosoftWindowsServer -offer WindowsServer
-# Get-AzVMImagePublisher -location eastus2 | where publishername -contains canonical
-# Get-AzVMImageOffer -location eastus2 -publisher canonical
-#  Get-AzVMImageSku -location eastus2 -publisher canonical -offer UbuntuServer
-$VirtualMachine = Set-AzVMSourceImage `
-                        -VM $VirtualMachine `
-                        -PublisherName 'Canonical' `
-                        -Offer 'UbuntuServer' `
-                        -Skus '18_04-lts-gen2' `
-                        -Version latest 
-New-AzVM -ResourceGroupName $ResourceGroupName -Location $LocationName -VM $VirtualMachine -Verbose
+function Add-LinuxVM {
+    param
+  (
+    [string]$VMName,
+    [string]$VMSubnet,
+    [string]$VMAvailabilitySet,
+    [string]$VMUserdata,
+    [string]$VMKey
+  )
+    # constants
+    $LocationName = "eastus2"
+    $VMSize = "Standard_B1s"
+    $vnetName = "web-vnet"
+    # modified vars
+    $ComputerName = $VMName
+    $nicName = $VMName + "-nic"
+    # networking
+    $vnet = Get-AzVirtualNetwork -name $vnetName
+    $subnet1 = $vnet | Get-AzVirtualNetworkSubnetConfig | where-object {$_.name -eq $VMSubnet}
+    $NIC = New-AzNetworkInterface `
+        -Name $nicName -ResourceGroupName $ResourceGroupName `
+        -Location $LocationName -SubnetId $subnet1.Id -Force
+    # set up vm obj
+    $availSet = Get-AzAvailabilitySet -name $VMAvailabilitySet
+    $VirtualMachine = New-AzVMConfig `
+        -VMName $VMName -VMSize $VMSize `
+        -AvailabilitySetId $availSet.Id
+    # creds
+    $securePassword = ConvertTo-SecureString ' ' -AsPlainText -Force
+    $cred = New-Object System.Management.Automation.PSCredential ("azureuser", $securePassword)
+    $sshPublicKey = Get-Content $VMKey
+    # setup OS
+    $userdata = Get-Content $VMUserdata | out-string
+    $VirtualMachine = Set-AzVMOperatingSystem `
+            -VM $VirtualMachine -Linux -ComputerName $ComputerName `
+            -Credential $cred -DisablePasswordAuthentication `
+            -CustomData $userdata
+    Add-AzVMSshPublicKey `
+        -VM $VirtualMachine `
+        -KeyData $sshPublicKey `
+        -Path "/home/azureuser/.ssh/authorized_keys"
+    $VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -NetworkInterface $NIC
+    #  Get-AzVMImageSku -location eastus -publishername MicrosoftWindowsServer -offer WindowsServer 
+    #  Get-AzVMImageSku -location eastus -publishername MicrosoftWindowsServer -offer WindowsServer
+    # Get-AzVMImagePublisher -location eastus2 | where publishername -contains canonical
+    # Get-AzVMImageOffer -location eastus2 -publisher canonical
+    #  Get-AzVMImageSku -location eastus2 -publisher canonical -offer UbuntuServer
+    $VirtualMachine = Set-AzVMSourceImage `
+        -VM $VirtualMachine -PublisherName 'Canonical' `
+        -Offer 'UbuntuServer' -Skus '18_04-lts-gen2' -Version latest 
+
+    New-AzVM -ResourceGroupName $ResourceGroupName -Location $LocationName -VM $VirtualMachine -Verbose
+    # spit out private ip
+    $vm = get-azvm -name $VMName
+    Get-AzNetworkInterface | where-object {$_.VirtualMachine.Id -eq $vm.Id} | foreach-object {$_.IpConfigurations.PrivateIpAddress }
+}
+
+function Remove-VM {
+    param(
+        [string]$VMName
+    )
+    Remove-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName -Force
+}
